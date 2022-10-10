026ee76c9ac6745af678b74bacfb37e146dd1047
Restyled.io <commits@restyled.io>
2022-10-10 12:17:53 +0000

    Restyled by clang-format

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import "./App.css";
-import WelcomeScreen from "./components/welcomeScreen/welcomeScreen";
+
+import {useEffect, useState} from "react";
+
 import NavBar from "./components/navBar/navBar";
-import { useEffect, useState } from "react";
+import WelcomeScreen from "./components/welcomeScreen/welcomeScreen";
+
 function App() {
   const [loading, setLoading] = useState(true);
-  useEffect(() => {
-    setTimeout(() => setLoading(false), 3500);
-  }, []);
+  useEffect(() => { setTimeout(() => setLoading(false), 3500); }, []);
   return (
     <>
       {loading ? <WelcomeScreen /> : ""}
