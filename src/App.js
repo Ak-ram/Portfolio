9465fae3622f57c01d67ea3809e104d8126738fd
Akram Ashraf <akram.ashraf1026.sd@science.bsu.edu.eg>
2022-10-10 12:12:06 +0000

    Update 5 files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
 import WelcomeScreen from "./components/welcomeScreen/welcomeScreen";
+import NavBar from "./components/navBar/navBar";
+import { useEffect, useState } from "react";
 function App() {
-  return <WelcomeScreen />;
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 5000);
+  }, []);
+  return (
+    <>
+      {loading ? <WelcomeScreen /> : ""}
+      <NavBar />
+    </>
+  );
 }
 
 export default App;
