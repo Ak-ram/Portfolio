b188e97f258f7fb5f7904c64e56c90d24b1a1089
Akram Ashraf <akram.ashraf1026.sd@science.bsu.edu.eg>
2022-10-10 20:20:35 +0000

    Update blog.jsx and blog.scss

--- src/pages/blog/blog.jsx
+++ src/pages/blog/blog.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import SearchBox from "../../components/searchBox/searchBox";
 import Filters from "../../components/filters/filters";
+import { FaRegComment } from "react-icons/fa";
+import { AiOutlineLike } from "react-icons/ai";
 import "./blog.scss";
 const Blog = () => {
   return (
@@ -19,6 +21,26 @@ const Blog = () => {
           "Git",
         ]}
       />
+      <article>
+        <img
+          src={
+            "http://www.imgworlds.com/wp-content/uploads/2015/12/18-CONTACTUS-HEADER.jpg"
+          }
+        />
+        <div className="article-content">
+          <span className="time-of-read">05 Minutes Read</span>
+          <h5 className="article-headline">SASS: The code you want</h5>
+          <div className="article-traffic">
+            <div className="likes">
+              2.3K <AiOutlineLike />
+            </div>
+            <div className="comments">
+              33
+              <FaRegComment />
+            </div>
+          </div>
+        </div>
+      </article>
     </div>
   );
 };
