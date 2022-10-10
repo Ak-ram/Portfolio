63b314f4b5f1b6d6fed72cbc2d7b15fcb5f58690
Akram Ashraf <akram.ashraf1026.sd@science.bsu.edu.eg>
2022-10-10 19:00:03 +0000

    Update 3 files

--- src/pages/blog/blog.jsx
+++ src/pages/blog/blog.jsx
@@ -1,7 +1,23 @@
 import React from "react";
-
+import { FiSearch } from "react-icons/fi";
+import "./blog.scss";
 const Blog = () => {
-  return <h1>Blog </h1>;
+  return (
+    <div className="blog-page">
+      <h2>Blogs</h2>
+      <div className="search-box">
+        <input
+          className="search-input"
+          type={"search"}
+          placeholder="search blog..."
+        />
+        <button>
+          <FiSearch />
+          Search
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Blog;
