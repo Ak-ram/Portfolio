ea82512185d8f28f8573e6054ee2365f5f35f47f
Restyled.io <commits@restyled.io>
2022-10-10 16:46:23 +0000

    Restyled by clang-format

--- src/App.js
+++ src/App.js
@@ -1,30 +1,31 @@
 import "./App.css";
-import WelcomeScreen from "./components/welcomeScreen/welcomeScreen";
+
+import {useEffect, useState} from "react";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+
 import NavBar from "./components/navBar/navBar";
+import WelcomeScreen from "./components/welcomeScreen/welcomeScreen";
+import Blog from "./pages/blog/blog";
 import Home from "./pages/home/home.jsx";
 import Profile from "./pages/profile/profile";
 import Projects from "./pages/projects/projects";
-import Blog from "./pages/blog/blog";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { useEffect, useState } from "react";
+
 function App() {
   const [loading, setLoading] = useState(true);
-  useEffect(() => {
-    setTimeout(() => setLoading(false), 3500);
-  }, []);
-  let appContent = (
-    <Router>
-      <NavBar />
+  useEffect(() => { setTimeout(() => setLoading(false), 3500); }, []);
+  let appContent =
+      (<Router><NavBar />
 
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/projects" element={<Projects />}></Route>
-        <Route path="/blog" element={<Blog />}></Route>
-        <Route path="/profile" element={<Profile />}></Route>
-      </Routes>
-    </Router>
-  );
-  return <>{loading ? <WelcomeScreen /> : appContent}</>;
+       <Routes><Route path = "/" element = {<Home />}>
+       </Route>
+        <Route path="/projects " element={<Projects />}></Route>
+            < Route path = "/blog" element = {<Blog />}>
+       </Route>
+        <Route path="/profile " element={<Profile />}></Route>
+        < /Routes>
+    </Router>);
+  return <>{loading ? <WelcomeScreen />: appContent} <
+         />;
 }
 
 export default App;
