4f13cf28059a498c6c5badd3b5af8c731d91acfc
Akram Ashraf <akram.ashraf1026.sd@science.bsu.edu.eg>
2022-10-11 06:33:21 +0000

    Update newsletter.jsx and newsletter.scss

--- src/pages/newsletter/newsletter.jsx
+++ src/pages/newsletter/newsletter.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { AiOutlineSend } from "react-icons/ai";
+import { IoIosArrowBack } from "react-icons/io";
 import "./newsletter.scss";
 const Newsletter = () => {
   return (
     <section className="newsletter-container">
+      <IoIosArrowBack className="backIcon"/>
       <div className="newsletter">
         <div className="img-container">
           <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5GgmCmAaVkcpV1YyAHc0Xe7gxNTL39_AsGA&usqp=CAU" />
