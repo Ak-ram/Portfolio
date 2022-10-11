d590c5b3a9eaf9675517bfed7af912b01440028a
Akram Ashraf <akram.ashraf1026.sd@science.bsu.edu.eg>
2022-10-11 06:14:18 +0000

    Update newsletter.jsx and newsletter.scss

--- src/pages/newsletter/newsletter.jsx
+++ src/pages/newsletter/newsletter.jsx
@@ -1,7 +1,22 @@
 import React from "react";
-
+import "./newsletter.scss";
 const Newsletter = () => {
-  return <h1>Newsletter</h1>;
+  return (
+    <section className="newsletter-container">
+      <div className="newsletter">
+        <div className="img-container">
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5GgmCmAaVkcpV1YyAHc0Xe7gxNTL39_AsGA&usqp=CAU" />
+        </div>
+        <div className="description">
+          <h3>Subscribe</h3>
+          <p>Subscribe to our newsletter & get weekly news</p>
+        </div>
+        <div className="email-box">
+          <input type={"email"} placeholder="Enter Your Email" />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Newsletter;
