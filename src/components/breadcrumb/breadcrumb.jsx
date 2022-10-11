871a329def28fd649ce220cfd4233f3b59123937
Akram Ashraf <akram.ashraf1026.sd@science.bsu.edu.eg>
2022-10-11 07:01:11 +0000

    Update breadcrumb.jsx and breadcrumb.scss

--- src/components/breadcrumb/breadcrumb.jsx
+++ src/components/breadcrumb/breadcrumb.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { AiOutlineSend } from "react-icons/ai";
 import { IoIosArrowBack } from "react-icons/io";
+import { useNavigate } from "react-router-dom";
 import "./breadcrumb.scss";
 const Breadcrumb = () => {
+  const navigate = useNavigate();
   return (
-    <div className="breadcrumb-container">
+    <div className="breadcrumb-container" onClick={() => navigate(-1)}>
       <IoIosArrowBack className="backIcon" />
+      <span>back</span>
     </div>
   );
 };
