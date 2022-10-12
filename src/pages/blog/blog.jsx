1fc1c7a5c6f8b0d3630c07847ac2939485daf032
Akram Ashraf <akram.ashraf1026.sd@science.bsu.edu.eg>
2022-10-12 12:01:24 +0000

    Update blog.jsx

--- src/pages/blog/blog.jsx
+++ src/pages/blog/blog.jsx
@@ -16,7 +16,30 @@ const Blog = () => {
       setDevBlogs(response.data);
     });
   }, []);
-  if (!devBlogs) return <Loader />;
+
+  let blogs = devBlogs.map(
+    ({
+      cover_image,
+      id,
+      title,
+      comments_count,
+      positive_reactions_count,
+      reading_time_minutes,
+      readable_publish_date,
+      tag_list,
+    }) => (
+      <Article
+        key={id}
+        coverImage={cover_image}
+        title={title}
+        reactionsCount={positive_reactions_count}
+        commentsCount={comments_count}
+        readingTime={reading_time_minutes}
+        publishDate={readable_publish_date}
+        tagList={tag_list}
+      />
+    )
+  );
 
   return (
     <div className="blog-page">
@@ -40,31 +63,7 @@ const Blog = () => {
           "git",
         ]}
       />
-      <div className="articles-container">
-        {devBlogs?.map(
-          ({
-            cover_image,
-            id,
-            title,
-            comments_count,
-            positive_reactions_count,
-            reading_time_minutes,
-            readable_publish_date,
-            tag_list,
-          }) => (
-            <Article
-              key={id}
-              coverImage={cover_image}
-              title={title}
-              reactionsCount={positive_reactions_count}
-              commentsCount={comments_count}
-              readingTime={reading_time_minutes}
-              publishDate={readable_publish_date}
-              tagList={tag_list}
-            />
-          )
-        )}
-      </div>
+      <div className="articles-container">{devBlogs ? blogs : <Loader />}</div>
     </div>
   );
 };
