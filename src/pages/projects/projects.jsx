1a5c0364baf40a854f73cf5300ea99a4cdb82c8b
Akram Ashraf <akram.ashraf1026.sd@science.bsu.edu.eg>
2022-10-12 08:31:31 +0000

    Update projects.jsx and projects.scss

--- src/pages/projects/projects.jsx
+++ src/pages/projects/projects.jsx
@@ -3,18 +3,45 @@ import ProjectsStatsBox from "../../components/projectsStatsBox/projectsStatsBox
 
 import "./projects.scss";
 const Projects = () => {
-
-
   return (
     <>
       <div className="projects-page">
         <h2>Projects</h2>
         <ProjectsStatsBox />
         <section className="technologyes">
-          <h3>Technologies</h3>
-          {/* <div className="technologyBox-container">
-           
-          </div> */}
+          <h3>Projects List</h3>
+          <div className="projects-slider">
+            <div className="project">
+              <img
+                src="https://items.epicpxls.com/uploads/photo/ced316c22c23d8215bf0ebfae3fddb22.jpg"
+                alt="moviideck"
+              />
+              <div className="project-details">
+                <h4>Moviideck</h4>
+                <span>React, Js, Html</span>
+              </div>
+            </div>
+            <div className="project">
+              <img
+                src="https://items.epicpxls.com/uploads/photo/ced316c22c23d8215bf0ebfae3fddb22.jpg"
+                alt="moviideck"
+              />
+              <div className="project-details">
+                <h4>Moviideck</h4>
+                <span>React, Js, Html</span>
+              </div>
+            </div>
+            <div className="project">
+              <img
+                src="https://items.epicpxls.com/uploads/photo/ced316c22c23d8215bf0ebfae3fddb22.jpg"
+                alt="moviideck"
+              />
+              <div className="project-details">
+                <h4>Moviideck</h4>
+                <span>React, Js, Html</span>
+              </div>
+            </div>
+          </div>
         </section>
       </div>
     </>
