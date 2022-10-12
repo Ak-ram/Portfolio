7a31311a3b5d50e7e8fd39a6022447600af3e0bc
Akram Ashraf <akram.ashraf1026.sd@science.bsu.edu.eg>
2022-10-12 11:57:25 +0000

    Update 3 files

--- src/components/projectsSlider/projectsSlider.jsx
+++ src/components/projectsSlider/projectsSlider.jsx
@@ -2,25 +2,18 @@ import React from "react";
 import Loader from "../../components/loader/loader";
 import "./projectsSlider.scss";
 
-const ProjectsSlider = () => {
-  const [projectImgs, setProjectImgs] = useState(null);
-  const baseURL = "http://myjson.dit.upm.es/api/bins/e06w";
-
-  useEffect(() => {
-    axios.get(baseURL).then((response) => {
-      setProjectImgs(response.data);
-    });
-  }, []);
-  if (!projectImgs) return <Loader />;
+const ProjectsSlider = ({ projects }) => {
+  if (!projects) return <Loader />;
 
   return (
     <div className="projects-slider">
-      <div className="project">
-        <img
-          src="https://assets-global.website-files.com/6009ec8cda7f305645c9d91b/60107f6dc0c29a14c9eb3d56_6002086f72b7276fe601e2b5_louie-steller.gif"
-          alt="moviideck"
-        />
-      </div>
+      {projects.map((project) => {
+        return (
+          <div className="project">
+            <img src={Object.values(project)} alt={Object.keys(project)} />
+          </div>
+        );
+      })}
     </div>
   );
 };
