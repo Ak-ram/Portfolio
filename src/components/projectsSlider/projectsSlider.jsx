b21921cdf98c537a52f0dc8675642377638532ec
Akram Ashraf <akram.ashraf1026.sd@science.bsu.edu.eg>
2022-10-12 15:31:11 +0000

    Update projectsSlider.jsx and projectsSlider.scss

--- src/components/projectsSlider/projectsSlider.jsx
+++ src/components/projectsSlider/projectsSlider.jsx
@@ -1,13 +1,27 @@
 import React from "react";
 import Loader from "../../components/loader/loader";
-import { FaChevronRight } from "react-icons/fa";
+import { FaChevronRight, FaChevronLeft } from "react-icons/fa";
 import "./projectsSlider.scss";
+import { useRef } from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 
 const ProjectsSlider = ({ projects }) => {
+  const projectsSliderRef = useRef();
+  const [Xoffset, setXoffset] = useState(0);
+  useEffect(() => {
+    projectsSliderRef.current.onscroll = () => {
+      let scrollAmount = projectsSliderRef.current.scrollLeft;
+      setXoffset((prev) =>
+        prev >= scrollAmount ? console.log("back") : console.log("forward")
+      );
+      // console.log(Xoffset);
+    };
+  }, [Xoffset]);
   if (!projects?.length) return <Loader />;
 
   return (
-    <div className="projects-slider">
+    <div className="projects-slider" ref={projectsSliderRef}>
       {projects.map((project) => {
         return (
           <div key={Object.keys(project)} className="project">
@@ -16,6 +30,7 @@ const ProjectsSlider = ({ projects }) => {
         );
       })}
       <FaChevronRight className="nextArrow" />
+      <FaChevronLeft className="prevArrow" />
     </div>
   );
 };
