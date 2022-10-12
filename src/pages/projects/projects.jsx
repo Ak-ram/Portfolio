a685ca37ba9d0df04700e46656063e07ba9c23fc
Akram Ashraf <akram.ashraf1026.sd@science.bsu.edu.eg>
2022-10-12 10:53:55 +0000

    Update projects.jsx

--- src/pages/projects/projects.jsx
+++ src/pages/projects/projects.jsx
@@ -31,7 +31,7 @@ const Projects = () => {
                 <span>React, Js, Html</span>
               </div>
             </div>
-            
+
             <div className="project">
               <img
                 src="https://extendthemes.com/wp-content/uploads/2018/06/great.gif"
@@ -42,7 +42,7 @@ const Projects = () => {
                 <span>React, Js, Html</span>
               </div>
             </div>
-                   <div className="project">
+            <div className="project">
               <img
                 src="https://assets-global.website-files.com/6009ec8cda7f305645c9d91b/60107f6d2368c291a6ab4992_6002086f72b72705be01e2c5_StVwfpeF7eIFN6jqqKVqEnD77WJV8qxkG39DEBtS3ZJyTf6gz2CAkReH-JbFG4_P009xYimtRJ49W_9QKIb9LZZBHkPFwlRs_bUQPRgIayepfBo5DNEJSBoFkQ9-qQSzT38rmglU.gif"
                 alt="moviideck"
@@ -52,10 +52,21 @@ const Projects = () => {
                 <span>React, Js, Html</span>
               </div>
             </div>
-            
-             <div className="project">
+
+            <div className="project">
               <img
                 src="https://assets-global.website-files.com/6009ec8cda7f305645c9d91b/60107f6fdd7ce834543ca3b1_6002086f72b727842001e2de_hSdQz9B6-x9R-qtVE2_0Q-PFVl-erXJvz9xgNAv51sASE2kC9gpEGWMlsutEGcdv_yIvZwYGrh7hGUjiDvFxmiuIeoidWNNX7FVj5rxoLiH6LZVuOJ6HfZJCv5JaOkjdXf3z9Uix.gif"
+                alt="moviideck"
+              />
+              <div className="project-details">
+                <h4>Moviideck</h4>
+                <span>React, Js, Html</span>
+              </div>
+            </div>
+
+            <div className="project">
+              <img
+                src="https://assets-global.website-files.com/6009ec8cda7f305645c9d91b/60107f6dc0c29a14c9eb3d56_6002086f72b7276fe601e2b5_louie-steller.gif"
                 alt="moviideck"
               />
               <div className="project-details">
