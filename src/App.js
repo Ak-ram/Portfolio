406f6e0954b7e6a395362e616b34deb92377c8f0
Restyled.io <commits@restyled.io>
2022-10-12 12:36:37 +0000

    Restyled by prettier

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import "./App.css";
 
-import {useEffect, useState} from "react";
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import { useEffect, useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import NavBar from "./components/navBar/navBar";
 import WelcomeScreen from "./components/welcomeScreen/welcomeScreen";
-import {DevBlogsContextProvider} from "./contexts/DEVAPIContext";
+import { DevBlogsContextProvider } from "./contexts/DEVAPIContext";
 import Blog from "./pages/blog/blog";
 import Home from "./pages/home/home.jsx";
 import Newsletter from "./pages/newsletter/newsletter.jsx";
@@ -14,23 +14,29 @@ import Projects from "./pages/projects/projects";
 
 function App() {
   const [loading, setLoading] = useState(true);
-  useEffect(() => { setTimeout(() => setLoading(false), 3500); }, []);
-  let appContent =
-      (<Router><DevBlogsContextProvider><NavBar />
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 3500);
+  }, []);
+  let appContent = (
+    <Router>
+      <DevBlogsContextProvider>
+        <NavBar />
 
-       <Routes><Route path = "/" element = {<Home />}>
-       </Route>
-          <Route path="/projects
-        " element={<Projects />}></Route>
-            < Route path = "/blog" element = {<Blog />}>
-       </Route>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route
+            path="/projects
+        "
+            element={<Projects />}
+          ></Route>
+          <Route path="/blog" element={<Blog />}></Route>
           <Route path="/profile " element={<Profile />}></Route>
-            < Route path = "/newsletter" element = {<Newsletter />}>
-       </Route>
-        </Routes></DevBlogsContextProvider>
-    </Router>);
-  return <>{loading ? <WelcomeScreen />: appContent} <
-         />;
+          <Route path="/newsletter" element={<Newsletter />}></Route>
+        </Routes>
+      </DevBlogsContextProvider>
+    </Router>
+  );
+  return <>{loading ? <WelcomeScreen /> : appContent} </>;
 }
 
 export default App;
