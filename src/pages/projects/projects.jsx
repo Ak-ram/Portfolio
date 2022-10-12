d4b94b2bacde83b991af8a02890465f1574dd450
Akram Ashraf <akram.ashraf1026.sd@science.bsu.edu.eg>
2022-10-12 10:53:09 +0000

    Update projects.jsx

--- src/pages/projects/projects.jsx
+++ src/pages/projects/projects.jsx
@@ -13,7 +13,7 @@ const Projects = () => {
           <div className="projects-slider">
             <div className="project">
               <img
-                src="https://miro.medium.com/max/1400/0*HZZrSYhmFsITWBC8.jpg"
+                src="https://99designs-blog.imgix.net/blog/wp-content/uploads/2021/04/image_processing20201217-23502-cx8xv8.gif?auto=format&q=60&fit=max&w=930"
                 alt="moviideck"
               />
               <div className="project-details">
@@ -23,7 +23,7 @@ const Projects = () => {
             </div>
             <div className="project">
               <img
-                src="https://i.ytimg.com/vi/eTfVcagDbPg/hqdefault.jpg"
+                src="https://htmlburger.com/blog/wp-content/uploads/2022/04/Contact-US-Page-Example-Freelance-Web-Developer-Portfolio-Contact-Page.gif"
                 alt="moviideck"
               />
               <div className="project-details">
@@ -31,9 +31,31 @@ const Projects = () => {
                 <span>React, Js, Html</span>
               </div>
             </div>
+            
             <div className="project">
               <img
-                src="https://static.wixstatic.com/media/c0fd69_7aea5f068bf244d382426d164f1150b4~mv2.gif/v1/fill/w_320,h_240,q_90/c0fd69_7aea5f068bf244d382426d164f1150b4~mv2.gif"
+                src="https://extendthemes.com/wp-content/uploads/2018/06/great.gif"
+                alt="moviideck"
+              />
+              <div className="project-details">
+                <h4>Moviideck</h4>
+                <span>React, Js, Html</span>
+              </div>
+            </div>
+                   <div className="project">
+              <img
+                src="https://assets-global.website-files.com/6009ec8cda7f305645c9d91b/60107f6d2368c291a6ab4992_6002086f72b72705be01e2c5_StVwfpeF7eIFN6jqqKVqEnD77WJV8qxkG39DEBtS3ZJyTf6gz2CAkReH-JbFG4_P009xYimtRJ49W_9QKIb9LZZBHkPFwlRs_bUQPRgIayepfBo5DNEJSBoFkQ9-qQSzT38rmglU.gif"
+                alt="moviideck"
+              />
+              <div className="project-details">
+                <h4>Moviideck</h4>
+                <span>React, Js, Html</span>
+              </div>
+            </div>
+            
+             <div className="project">
+              <img
+                src="https://assets-global.website-files.com/6009ec8cda7f305645c9d91b/60107f6fdd7ce834543ca3b1_6002086f72b727842001e2de_hSdQz9B6-x9R-qtVE2_0Q-PFVl-erXJvz9xgNAv51sASE2kC9gpEGWMlsutEGcdv_yIvZwYGrh7hGUjiDvFxmiuIeoidWNNX7FVj5rxoLiH6LZVuOJ6HfZJCv5JaOkjdXf3z9Uix.gif"
                 alt="moviideck"
               />
               <div className="project-details">
