c3002e937de01d57522660c71941a205ebbc353c
Akram Ashraf <akram.ashraf1026.sd@science.bsu.edu.eg>
2022-10-13 08:50:56 +0000

    Update projectsStatsBox.jsx

--- src/components/projectsStatsBox/projectsStatsBox.jsx
+++ src/components/projectsStatsBox/projectsStatsBox.jsx
@@ -15,6 +15,7 @@ const ProjectsStatsBox = () => {
   const reactColor = "rgb(0 160 240)";
   const jsColor = "#F0DB4F";
   const vscColor = "#ff419d";
+  const othersColor = "#8585859c";
   return (
     <div className="projects-stats">
       <div className="leftSide">
@@ -36,7 +37,12 @@ const ProjectsStatsBox = () => {
               reactProjectsArray?.length * (360 / gitHubRepos.length) +
               vscodeExtensionGitHubReposArray?.length *
                 (360 / gitHubRepos.length)
-            }deg)`,
+            }deg, ${othersColor} ${
+              jsGitHubReposArray?.length * (360 / gitHubRepos.length) +
+              reactProjectsArray?.length * (360 / gitHubRepos.length) +
+              vscodeExtensionGitHubReposArray?.length *
+                (360 / gitHubRepos.length)
+            }deg )`,
           }}
         >
           <div className="circle-content">
@@ -52,7 +58,7 @@ const ProjectsStatsBox = () => {
         <div className="bottom">
           <div className="index">
             <span
-              className="key key-1"
+              className="key"
               style={{ background: `${reactColor}` }}
             ></span>
             <span className="technology">React</span>
@@ -60,22 +66,30 @@ const ProjectsStatsBox = () => {
           </div>
 
           <div className="index">
-            <span
-              className="key key-2"
-              style={{ background: `${jsColor}` }}
-            ></span>
+            <span className="key" style={{ background: `${jsColor}` }}></span>
             <span className="technology">JS</span>
             <span className="projectsCount">{jsGitHubReposArray.length}</span>
           </div>
 
           <div className="index">
-            <span
-              className="key key-3"
-              style={{ background: `${vscColor}` }}
-            ></span>
+            <span className="key" style={{ background: `${vscColor}` }}></span>
             <span className="technology">VS code</span>
             <span className="projectsCount">
               {vscodeExtensionGitHubReposArray.length}
+            </span>
+          </div>
+
+          <div className="index">
+            <span
+              className="key"
+              style={{ background: `${othersColor}` }}
+            ></span>
+            <span className="technology">Others</span>
+            <span className="projectsCount">
+              {gitHubRepos.length -
+                (vscodeExtensionGitHubReposArray.length +
+                  jsGitHubReposArray.length +
+                  reactProjectsArray.length)}
             </span>
           </div>
         </div>
