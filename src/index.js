afbd916cd87e328e3071d989a9a36ccacb17de9e
Restyled.io <commits@restyled.io>
2022-10-15 23:19:28 +0000

    Restyled by clang-format

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
+import "./styles/all.scss";
+
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./styles/all.scss";
+
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
