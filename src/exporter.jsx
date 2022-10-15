65e8ad5dc5cc85e5e01be58dcf95f9f796163999
Akram Ashraf <akram.ashraf1026.sd@science.bsu.edu.eg>
2022-10-15 22:36:27 +0000

    Create exporter.jsx

---
+++ src/exporter.jsx
@@ -0,0 +1,12 @@
+// components
+export { default as Article } from "./components/article/article";
+export { default as Breadcrumb } from "./components/breadcrumb/breadcrumb";
+export { default as Filters } from "./components/filters/filters";
+export { default as Loader } from "./components/loader/loader";
+export { default as Navbar } from "./components/navbar/navbar";
+export { default as ProjectCard } from "./components/projectCard/projectCard";
+export { default as ProjectSlider } from "./components/projectSlider/projectSlider";
+export { default as ProjectSliderBox } from "./components/projectSliderBox/projectSliderBox";
+export { default as SearchBox } from "./components/searchBox/searchBox";
+export { default as TechnologyBox } from "./components/technologyBox/technologyBox";
+export { default as WelcomeScreen } from "./components/welcomeScreen/welcomeScreen";
