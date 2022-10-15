c68949960ccc25204132af6affd2a1c739aab00f
Akram Ashraf <akram.ashraf1026.sd@science.bsu.edu.eg>
2022-10-15 22:40:48 +0000

    Update exporter.jsx

--- src/exporter.jsx
+++ src/exporter.jsx
@@ -10,3 +10,21 @@ export { default as ProjectSliderBox } from "./components/projectSliderBox/proje
 export { default as SearchBox } from "./components/searchBox/searchBox";
 export { default as TechnologyBox } from "./components/technologyBox/technologyBox";
 export { default as WelcomeScreen } from "./components/welcomeScreen/welcomeScreen";
+
+// contexts
+
+// pages
+
+// icons
+
+export { default as FaRegComment } from "react-icons/fa";
+export {
+  default as AiOutlineLike,
+  default as AiOutlineSend,
+} from "react-icons/ai";
+export { default as MdOutlineUpdate } from "react-icons/md";
+export { default as IoIosArrowBack } from "react-icons/io";
+export { default as ImHome } from "react-icons/im";
+import { GoLinkExternal } from "react-icons/go";
+import { SiGithub } from "react-icons/si";
+import { FiSearch } from "react-icons/fi";
