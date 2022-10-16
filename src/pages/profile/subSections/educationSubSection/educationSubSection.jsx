9fb639e9c871c2a95645594391cfc2e1901cdc10
Akram Ashraf <akram.ashraf1026.sd@science.bsu.edu.eg>
2022-10-16 17:18:48 +0000

    Update 3 files

--- src/pages/profile/subSections/educationSubSection/educationSubSection.jsx
+++ src/pages/profile/subSections/educationSubSection/educationSubSection.jsx
@@ -1,5 +1,7 @@
 import "./educationSubSection.scss";
 import { FaUniversity, FcGraduationCap } from "../../../../exporter";
+import udacityImg from "../../../../assests/udacity.png";
+import bsuImg from "../../../../assests/bsu.png";
 const EducationSubSection = () => {
   return (
     <div className="education">
@@ -12,23 +14,29 @@ const EducationSubSection = () => {
             {/* <TbNumber3 color="rgb(0 160 240)" size={20} /> */}
             <span>2019:2022</span>
           </span>
-          <div className="school-details">
+          <div
+            className="school-details"
+            style={{ backgroundImage: `url(${udacityImg})` }}
+          >
             <div className="school-name">
-              <FaUniversity size={15} /> BSc, Beni-Suef University.
+              <FaUniversity size={15} /> fwd, udacity Program.
             </div>
             <ul className="more-details">
               <li>
-                <b>Grade : </b> 3.23/5 ( V.Good )
+                <b>Grade : </b> Nanodegree
               </li>
             </ul>
           </div>
         </div>
-         <div className="school bsu">
+        <div className="school bsu">
           <span className="school-date">
             {/* <TbNumber3 color="rgb(0 160 240)" size={20} /> */}
             <span>2019:2022</span>
           </span>
-          <div className="school-details">
+          <div
+            className="school-details"
+            style={{ backgroundImage: `url(${bsuImg})` }}
+          >
             <div className="school-name">
               <FaUniversity size={15} /> BSc, Beni-Suef University.
             </div>
