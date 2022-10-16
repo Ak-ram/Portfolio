66a1097ce2707ca986e557d15f50c06b548daac6
Akram Ashraf <akram.ashraf1026.sd@science.bsu.edu.eg>
2022-10-16 14:12:20 +0000

    Update educationSubSection.jsx and educationSubSection.scss

--- src/pages/profile/subSections/educationSubSection/educationSubSection.jsx
+++ src/pages/profile/subSections/educationSubSection/educationSubSection.jsx
@@ -4,8 +4,14 @@ const EducationSubSection = () => {
     <div className="education">
       <h5>🎓 Education</h5>
       <div className="school-container">
-        <div className="school udacity"></div>
-        <div className="school bsu"></div>
+        <div className="school bsu">
+          <span className="school-date">2019:2022</span>
+          <div className="school-details"></div>
+        </div>
+        <div className="school bsu">
+          <span className="school-date">2019:2022</span>
+          <div className="school-details"></div>
+        </div>
       </div>
     </div>
   );
