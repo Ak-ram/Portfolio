7c8def6e7eff6ae7c9a171683ef1d3bb1dfb555c
Akram Ashraf <akram.ashraf1026.sd@science.bsu.edu.eg>
2022-10-16 16:35:06 +0000

    Update educationSubSection.jsx and educationSubSection.scss

--- src/pages/profile/subSections/educationSubSection/educationSubSection.jsx
+++ src/pages/profile/subSections/educationSubSection/educationSubSection.jsx
@@ -16,6 +16,14 @@ const EducationSubSection = () => {
             <div className="school-name">
               <FaUniversity size={15} /> BSc, Beni-Suef University.
             </div>
+            <ul className="more-details">
+              <li>
+                <b>Grade: </b>3.23/5 ( V.Good )
+              </li>
+              <li>
+                <b>Relevent Courses: </b>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
