a59946be04eff234f226d8f33735316c420a8cf7
Akram Ashraf <akram.ashraf1026.sd@science.bsu.edu.eg>
2022-10-18 15:08:40 +0000

    Update 4 files

--- src/App.js
+++ src/App.js
@@ -22,19 +22,21 @@ function App() {
     <Router>
       <DevBlogsContextProvider>
         <GitHubReposContextProvider>
-          <NavBar />
-          <Routes>
-            <Route path="/" element={<Profile />}></Route>
-            <Route path="/projects" element={<Projects />}></Route>
-            {/* Dynamic routing */}
-            <Route
-              path="projects/:projectName"
-              element={<ProjectDescription  />}
-            ></Route>
-            <Route path="/blog" element={<Blog />}></Route>
-            <Route path="/newsletter" element={<Newsletter />}></Route>
-            <Route path="/settings" element={<Settings />}></Route>
-          </Routes>
+          <div className="app dark-mode">
+            <NavBar />
+            <Routes>
+              <Route path="/" element={<Profile />}></Route>
+              <Route path="/projects" element={<Projects />}></Route>
+              {/* Dynamic routing */}
+              <Route
+                path="projects/:projectName"
+                element={<ProjectDescription />}
+              ></Route>
+              <Route path="/blog" element={<Blog />}></Route>
+              <Route path="/newsletter" element={<Newsletter />}></Route>
+              <Route path="/settings" element={<Settings />}></Route>
+            </Routes>
+          </div>
         </GitHubReposContextProvider>
       </DevBlogsContextProvider>
     </Router>
