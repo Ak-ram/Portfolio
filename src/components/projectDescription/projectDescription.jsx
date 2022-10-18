4b1fe115b8809849ba558dcaf6dcc62ff0dc82b2
Akram Ashraf <akram.ashraf1026.sd@science.bsu.edu.eg>
2022-10-18 11:07:51 +0000

    Update projectDescription.jsx and projectDescription.scss

--- src/components/projectDescription/projectDescription.jsx
+++ src/components/projectDescription/projectDescription.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import { Outlet, useParams } from "react-router-dom";
 import { useGitHubRepos } from "../../exporter";
+import Breadcrumb from "../breadcrumb/breadcrumb";
+import avatar from "../../assests/avatar.webp";
 import "./projectDescription.scss";
 const ProjectDescription = () => {
   const { gitHubRepos } = useGitHubRepos();
@@ -11,14 +13,40 @@ const ProjectDescription = () => {
     gitHubRepos?.find((repo) => {
       if (repo.name.toLowerCase() === params.projectName) {
         setCurrentRepo(repo);
+        console.log(repo);
       }
     });
   }, []);
 
   return (
     <>
-      <h1>{currentRepo.id}</h1>
-      <p>{currentRepo.description}</p>
+      <div className="details-page">
+        <div className="banner">
+          <header className="controls">
+            <Breadcrumb />
+          </header>
+          <div className="page-main-details">
+            <div className="publish-info">
+              <div className="publisher">
+                <img
+                  width={50}
+                  src={currentRepo?.owner?.avatar_url}
+                  alt="publisher-image"
+                />
+                <span className="publish-date">29/2022</span>
+              </div>
+              <div className="headline">
+                <h2>{currentRepo?.name}</h2>
+                <footer>
+                  <span>By {currentRepo?.owner?.login}</span>
+                  <span>5 Minutes Read</span>
+                </footer>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* <p>{currentRepo.description}</p> */}
       {/* <Outlet /> */}
     </>
   );
